c2a8b2d339d757718288eb040e514564f0554b5e
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-19 08:02:51 -0700

    file updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Using Puppet, create a file in /t .mp.
+# Using Puppet, create a file in /tmp.
+
 file { '/tmp/school':
-    ensure  => 'file',
-    mode   => '0744',
-    group   => 'www-data',
-    owner    => 'www-data',
-    content => 'I love Puppet',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
