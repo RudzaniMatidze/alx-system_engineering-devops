a0201fe5f12b44ee0386f40d2f09eb17ecdd5eb9
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-20 10:08:05 -0700

    file updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
- Python script that, using this REST API, for a given employee ID,
- returns information about his/her TODO list progress.
-"""
+"""Access a REST API for todo list of employees"""
 
 import requests
 import sys
