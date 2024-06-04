f2be221641eaf1d264c378e09857da083a766ece
RudzaniMatidze <rdzmatidze@gmail.com>
2024-06-04 05:12:29 -0700

    queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    A function that Queries the Reddit API and returns the total number
+    of subscribers for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_aget = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
